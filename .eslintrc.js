1b050431294630e3ce64b325cf43f0059f2fc5c9
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-04-30 11:15:00 +0300

    refactor: :wrench: Update configuration files

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,14 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+    "vue/html-closing-bracket-newline": [
+      "error",
+      {
+        singleline: "never",
+        multiline: "always",
+      },
+    ],
   },
   overrides: [
     {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "brand-gray-1": "#dadce0",
+        "brand-blue-1": "#1967d2",
+        "brand-green-1": "#137333",
+      },
+    },
   },
   plugins: [],
 };
