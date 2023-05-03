7f761c896888cd2f67e9dff98f7ca0614f402465
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-05-03 19:52:07 +0300

    test: :test_tube: Add test suites for Navigation components

---
+++ tests/unit/components/ProfileImage.test.js
@@ -0,0 +1,10 @@
+import { shallowMount } from "@vue/test-utils";
+import ProfileImage from "@/components/ProfileImage.vue";
+import { describe, it, expect } from "@jest/globals";
+
+describe("ProfileImage.vue", () => {
+  it("displays user profile image", () => {
+    const wrapper = shallowMount(ProfileImage);
+    expect(wrapper.exists()).toBe(true);
+  });
+});
---
+++ tests/unit/components/SubNav.test.js
@@ -0,0 +1,40 @@
+import { describe, it, expect } from "@jest/globals";
+import { mount } from "@vue/test-utils";
+import SubNav from "@/components/SubNav.vue";
+
+describe("SubNav.vue", () => {
+  describe("when  user is on job page", () => {
+    it("displays the job count", () => {
+      const wrapper = mount(SubNav, {
+        data() {
+          return {
+            onJobResultsPage: true,
+          };
+        },
+      });
+      const jobCount = wrapper.find("[data-test='job-count']");
+      expect(jobCount.exists()).toBe(true);
+    });
+  });
+
+  describe("when  user is not on job page", () => {
+    it("doesn't display the job count", () => {
+      const wrapper = mount(SubNav, {
+        // global object is used to mock global components
+        global: {
+          // stubs used to be a replacement for FontAwesomeIcon
+          stubs: {
+            FontAwesomeIcon: true,
+          },
+        },
+        data() {
+          return {
+            onJobResultsPage: false,
+          };
+        },
+      });
+      const jobCount = wrapper.find("[data-test='job-count']");
+      expect(jobCount.exists()).toBe(false);
+    });
+  });
+});
