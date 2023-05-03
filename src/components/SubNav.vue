cc21ae37c0120056363462964b35684542d0ecbf
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-05-03 19:49:21 +0300

    feat: :sparkles: Add SubNav component + FontAwesomeIcon as global component

---
+++ src/components/SubNav.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="w-full h-16 bg-white border-b border-solid border-brand-gray-1">
+    <div class="flex items-center h-full px-8">
+      <div v-if="onJobResultsPage" data-test="job-count">
+        <!-- icon prop used to customize the actual icon -->
+        <font-awesome-icon
+          class="mr-3 text-gray-600 text-base"
+          :icon="['fas', 'search']"
+        />
+        <span>
+          <span class="text-brand-green-1">1276</span>
+          jobs matched
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SubNav",
+  data() {
+    return {
+      onJobResultsPage: true,
+    };
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from "vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
 import App from "@/App.vue";
 import "./assets/tailwind.css";
 
-createApp(App).mount("#app");
+library.add(faSearch);
+
+// createApp(App) creates a new Vue instance and mounts it to the #app element.
+// component("font-awesome-icon", FontAwesomeIcon) registers the FontAwesomeIcon component globally.
+// mount("#app") mounts the Vue instance to the #app element.
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
