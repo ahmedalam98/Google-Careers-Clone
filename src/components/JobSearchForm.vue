e30db5a240d5e73f6af3e1c6c8d6e49376857fd4
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-05-03 19:50:44 +0300

    feat: :sparkles: Add JobSearchForm component

---
+++ src/components/JobSearchForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <form
+    class="flex items-center w-full h-12 mt-14 border border-solid border-brand-gray-3 rounded-3xl"
+  >
+    <font-awesome-icon
+      class="ml-4 mr-3 text-gray-600"
+      :icon="['fas', 'search']"
+    />
+    <div class="flex flex-nowrap flex-1 h-full text-base font-light">
+      <div class="relative flex items-center flex-1 h-full pr-3">
+        <label class="absolute left-0 -top-10 font-normal">Role</label>
+        <input
+          type="text"
+          v-model="role"
+          placeholder="Software engineer"
+          class="w-full text-lg font-normal focus:outline-none"
+        />
+      </div>
+
+      <span
+        class="flex items-center h-full px-3 border-l border-r border-brand-gray-3 bg-brand-gray-2 font-normal"
+        >in</span
+      >
+      <div class="relative flex items-center flex-1 h-full pl-3">
+        <label class="absolute left-0 -top-10 font-normal">Where?</label>
+        <input
+          type="text"
+          v-model="location"
+          placeholder="Los Angeles"
+          class="w-full text-lg font-normal focus:outline-none"
+        />
+      </div>
+    </div>
+    <ActionButton text="Search" type="secondary" class="rounded-r-3xl" />
+  </form>
+</template>
+
+<script>
+import ActionButton from "./ActionButton.vue";
+export default {
+  name: "JobSearchForm",
+  components: {
+    ActionButton,
+  },
+  data: function () {
+    return {
+      role: "",
+      location: "",
+    };
+  },
+};
+</script>
