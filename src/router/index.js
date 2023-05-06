8c5836a8621d208b0f95cfc66e39802484d7679a
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-05-06 15:35:56 +0300

    feat: :sparkles: Add feat lazy loading + grouping webpack code chunks

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,16 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "@/views/HomeView.vue";
-import JobResultsView from "@/views/JobResultsView.vue";
+// import HomeView from "@/views/HomeView.vue";
+// import JobResultsView from "@/views/JobResultsView.vue";
+// import JobView from "@/views/JobView.vue";
+
+// Lazy Loading : dynamic import syntax to tell webpack that we want to split the code into separate chunks.
+const HomeView = () => import("@/views/HomeView.vue");
+
+// grouping lazy loaded components by creating a separate chunk for all the routes that are related to jobs.
+const JobResultsView = () =>
+  import(/* webpackChunkName: "jobs"*/ "@/views/JobResultsView.vue");
+const JobView = () =>
+  import(/* webpackChunkName: "jobs"*/ "@/views/JobView.vue");
 
 const routes = [
   // for each object in the array, ***we have path, name and component***.
@@ -16,6 +26,13 @@ const routes = [
     path: "/jobs/results",
     name: "JobResults",
     component: JobResultsView,
+  },
+  // id ---> is a **dynamic parameter** that we can use to pass the id of the job that we want to display.
+  // whatever we write after the colon will be the name of the parameter of ( this.$route.params.id ) in the component.
+  {
+    path: "/jobs/results/:id",
+    name: "JobListing",
+    component: JobView,
   },
 ];
 
