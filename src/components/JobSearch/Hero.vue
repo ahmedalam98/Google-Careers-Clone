82cc6bfb7b81f7d8b679fd7a4fc894537cf6ae78
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-05-10 23:14:00 +0300

    feat: :sparkles: Add spotlights to local DB + Render spotlight component in Home Page

--- src/components/JobSearch/Hero.vue
+++ src/components/JobSearch/Hero.vue
@@ -1,6 +1,6 @@
 <template>
-  <main>
-    <section class="flex flex-col h-screen pt-10 pb-20">
+  <main class="flex flex-col h-screen">
+    <section class="flex flex-col pt-10 pb-20">
       <div class="grid grid-cols-12">
         <div class="col-start-1 col-span-1"></div>
         <div class="col-start-2 col-span-5">
@@ -16,17 +16,53 @@
         <div class="col-start-12 col-span1"></div>
       </div>
     </section>
+
+    <!-----------Spotlight slot injection----------->
+    <Spotlight class="flex flex-row justify-center pb-16 px-4">
+      <!--slotProps is the object of data for spotlights, you can assign any name to it-->
+      <template #default="slotProps">
+        <router-link
+          to="/jobs/results"
+          class="flex flex-col mx-5 border rounded-lg w-72 bg-brand-gray-2 h-96"
+          :style="{ width: '600px', height: '300' }"
+        >
+          <div
+            class="flex flex-col justify-end h-full bg-cover"
+            :style="{ backgroundImage: 'url(' + slotProps.spotlight.img + ')' }"
+          >
+            <div class="text-white">
+              <div class="px-6 py-4">
+                <h3 class="text-3xl">{{ slotProps.spotlight.title }}</h3>
+                <p class="pt-4 text-xl">
+                  {{ slotProps.spotlight.description }}
+                </p>
+              </div>
+            </div>
+
+            <router-link
+              to="/jobs/results"
+              class="px-6 pb-4 text-sm text-white"
+            >
+              See jobs
+            </router-link>
+          </div>
+        </router-link>
+      </template>
+    </Spotlight>
   </main>
 </template>
 
 <script>
 import Headline from "./Headline.vue";
 import JobSearchForm from "./JobSearchForm.vue";
+import Spotlight from "./Spotlight.vue";
+
 export default {
   name: "Hero",
   components: {
     Headline,
     JobSearchForm,
+    Spotlight,
   },
 };
 </script>
---
+++ src/components/JobSearch/Spotlight.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li v-for="spotlight in spotlights" :key="spotlight.id">
+      <slot :spotlight="spotlight"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Spotlight",
+  data() {
+    return {
+      spotlights: [],
+    };
+  },
+  async mounted() {
+    const baseUrl = process.env.VUE_APP_API_URL;
+    const url = `${baseUrl}/spotlights`;
+    const response = await axios.get(url);
+    this.spotlights = response.data;
+  },
+};
+</script>
