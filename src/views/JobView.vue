ba27fea1adac787d68c3713b7fa97a93ff7fbb5e
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-05-14 11:12:37 +0300

    refactor: :recycle: Add JobView route

---
+++ src/views/JobView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex h-screen">
+    <div class="m-auto">
+      <p class="text-center text-3xl mb-28">
+        Job Route Parameter : {{ currentJobId }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "JobView",
+  computed: {
+    currentJobId() {
+      return this.$route.params.id;
+    },
+  },
+};
+</script>
